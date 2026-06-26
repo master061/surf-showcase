0ba85f233e5fd782c40abaffc10d82ec0a4bfbc9
master061 <3238405241@qq.com>
2026-06-27 00:16:55 +0800

    clean: 移除旧项目文件，只保留当前微信小程序代码

--- src/app.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react'
-import { AuthProvider } from './components/AuthStore'
-import './app.scss'
-
-function App({ children }: PropsWithChildren) {
-  return <AuthProvider>{children}</AuthProvider>
-}
-
-export default App
